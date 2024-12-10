3c51bdcfb819dc8e4eaa0a33ed99d1167c1965de
mistank <milan.stankovic550@gmail.com>
2024-12-11 00:39:08 +0100

    Removed tracking for some files

--- frontend/public/js/cljs-runtime/recipe_app.components.list.styles.js
+++
@@ -1,5 +0,0 @@
-goog.provide('recipe_app.components.list.styles');
-recipe_app.components.list.styles.list_style = new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"margin","margin",-995903681),"20px"], null);
-recipe_app.components.list.styles.recipe_item_style = new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"margin-bottom","margin-bottom",388334941),"10px"], null);
-
-//# sourceMappingURL=recipe_app.components.list.styles.js.map
--- frontend/public/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { recipe_app.core.init(); } catch (e) { console.error("An error occurred when calling (recipe-app.core/init)"); console.error(e); }
